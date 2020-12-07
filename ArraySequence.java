d2a72a180ab86ffd7f36cc6e94a944a0d3f12bf1
mzimic20 <>
2020-12-07 17:00:30 -0500

    progress

--- ArraySequence.java
+++ ArraySequence.java
@@ -6,7 +6,35 @@ public class ArraySequence implements IntegerSequence {
   private int[] data;
 
   public ArraySequence(int[] other) {
-    
+    currentIndex = 0;
+    data = new int[ other.length ];
+    for(int i = 0; i < other.length; i++) {
+      data[i] = other[i];
+    }
+  }
+
+  public ArraySequence(IntegerSequence otherseq) {
+    for(int i = 0; i < otherseq.length(); i++) {
+      data[i] = otherseq.next();
+    }
+  }
+
+  public void reset() {
+    currentIndex = 0;
+    data = new int[0];
+  }
+
+  public int length() {
+    return data.length;
+  }
+
+  public boolean hasNext() {
+    return currentIndex < data.length - 1;
+  }
+
+  public int next() {
+    currentIndex++;
+    return data[currentIndex];
   }
 
 }
--- Range.java
+++ Range.java
@@ -1,25 +1,35 @@
+import java.util.NoSuchElementException;
+
 public class Range implements IntegerSequence {
 
   private int start, end, current;
 
-  public Range(int start, int end) {
-
+  public Range(int s, int e) {
+    start = s;
+    end = e;
+    current = s;
   }
 
   public void reset() {
-
+    start = 0;
+    end = 0;
+    current = 0;
   }
 
   public int length() {
-
+    return end - start + 1;
   }
 
   public boolean hasNext() {
-
+    return current < end + 1;
   }
 
-  public int next() throws NoSuchElementException {
-    
+  public int next() {
+    if ( hasNext() ) {
+      current++;
+      return current - 1;
+    }
+    else throw new NoSuchElementException("NoSuchElementException: No elements beyond " + (current - 1));
   }
 
 }
