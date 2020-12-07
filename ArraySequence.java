3543c6768bbcddeef85864340a65c00e1bd4cd17
mzimic20 <>
2020-12-07 13:46:48 -0500

    ArraySequence added

---
+++ ArraySequence.java
@@ -0,0 +1,12 @@
+import java.util.NoSuchElementException;
+
+public class ArraySequence implements IntegerSequence {
+
+  private int currentIndex;
+  private int[] data;
+
+  public ArraySequence(int[] other) {
+    
+  }
+
+}
